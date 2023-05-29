d20d3973b93ca78303596ecfae06d19732f41f9c
nakatatsu77 <nakatatsu1177@gmail.com>
2023-05-29 17:31:58 +0900

    リファクタリング

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 let nav = document.querySelector("#navArea");
 let btn = document.querySelector(".toggle-btn");
-let mask = document.querySelector("#mask");
 
 btn.onclick = () => {
   nav.classList.toggle("open");
